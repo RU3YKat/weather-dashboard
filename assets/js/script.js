2b4e566be55d48709ca95ec3ccd743f2950988fb
insomniKat <ru3ykat@gmail.com>
2021-10-03 10:07:58 -0400

    add form functionality basic api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,37 @@
-var citySearchEl = document.querySelector("#city-search");
+var searchFormEl = document.querySelector("#search-form");
+var cityInputEl = document.querySelector("#city-input");
+var apiKey = "05e60bc8d4c87c37c95c7da73c943730";
+
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
-    console.log("yes!")
+    var city = cityInputEl
+        .value
+        .trim();
+    
+    if (city) {
+        getCurrentWeather(city);
+        getTimeMachine(city);
+        cityInputEl.value = "";
+    } else {
+        alert("Please enter a US city name.")
+    };
 };
 
-citySearchEl.addEventListener("submit", formSubmitHandler);
+var getCurrentWeather = function(city) {
+    console.log(city);
+    // get current weather by city name
+    var currentWeatherApi = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    console.log(currentWeatherApi);
+    fetch(currentWeatherApi).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+
+            })
+        }
+    })
+}
+
+var getTimeMachine = fucntion
+
+searchFormEl.addEventListener("submit", formSubmitHandler);
